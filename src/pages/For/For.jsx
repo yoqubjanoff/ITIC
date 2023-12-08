763ac76fbcc4d5ef4488e00d4374a7d17bf3ed63
Ulugbek121 <yoqubjanov1991@gmail.com>
2023-12-09 00:00:46 +0500

    gsap animation first pie done

--- src/pages/For/For.jsx
+++ src/pages/For/For.jsx
@@ -1,19 +1,73 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Container, HeadingOneFor, Paragraph } from "./styles";
 import globe from "../../assets/images/globe.png";
 import building from "../../assets/images/building.svg";
 import freelancer from "../../assets/images/freelancer.svg";
-const For=()=> {
+import gsap from "gsap";
+
+// ScrollTriggerni ham yuklab olish
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const For = () => {
+  const myImgRef = useRef();
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: myImgRef.current,
+        start: "top center",
+        end: "bottom center",
+        scrub: 1,
+        toggleAction: "restart none none none", // restart, yuqori tomoni, past tomoni, yonida
+      },
+    });
+
+    // Qo'shilgan qismi animatsiyasini boshlash
+    tl.fromTo(
+      ".forImgGlobe",
+      { y: 350, width: 0, opacity: 0 },
+      { y: 30, width: "281px", opacity: 1, ease: "power2.out", duration: 1 }
+    );
+
+    tl.fromTo(
+      ".buildingImg",
+      { y: 350, width: 0, opacity: 0 },
+      {
+        y: 0,
+        width: "281px",
+        opacity: 1,
+        ease: "power2.out",
+        duration: 1,
+      },
+      "<"
+    );
+
+    tl.fromTo(
+      ".personImg",
+      { y: 350, width: 0, opacity: 0 },
+      {
+        y: 0,
+        width: "281px",
+        opacity: 1,
+        ease: "power2.out",
+        duration: 1,
+      },
+      "<"
+    );
+    return () => tl.kill();
+  }, []);
+
   return (
-    <Container id="services">
+    <Container id="services" ref={myImgRef}>
       <Container.Wrapper>
-        <Container.Box>
+        <Container.Box >
           <Container.Text>
             <Container.Number>01</Container.Number>
             <HeadingOneFor>Global Customers</HeadingOneFor>
             <Paragraph>
-              Connect with growing workforce and companies in the field of
-              software development and animation to ensure quality and
+              Connect with a growing workforce and companies in the field of
+              software development and animation to ensure the quality and
               competitive price of the final product
             </Paragraph>
           </Container.Text>
@@ -32,18 +86,20 @@ const For=()=> {
           <img className="buildingImg" src={building} alt="for" />
         </Container.Box>
 
-        <Container.Box>
+        <Container.Box >
           <Container.Text>
             <Container.Number>03</Container.Number>
             <HeadingOneFor>Freelancers</HeadingOneFor>
-            <Paragraph max-width="495px">Become a member of a freelancer's pool and work on creative projects</Paragraph>
+            <Paragraph max-width="495px">
+              Become a member of a freelancer's pool and work on creative
+              projects
+            </Paragraph>
           </Container.Text>
           <img className="personImg" src={freelancer} alt="for" />
         </Container.Box>
-
       </Container.Wrapper>
     </Container>
   );
-}
+};
 
 export default For;
